97b367cc410089aa7b14009aa10e815bf213ce9c
dahlia <dahlia@9813eb9d-5b34-4e77-9090-aba0a1003298>
2008-11-15 22:10:40 +0000

    Changed sculpty LOD scaling to use bilinear instead of nearest neighbor or highQualityBilinear. This seems to give the best results for balancing sculpty mesh quality and speed

--- trunk/IdealistViewer/Application/SculptMeshLOD.cs
+++ trunk/IdealistViewer/Application/SculptMeshLOD.cs
@@ -100,7 +100,7 @@ namespace IdealistViewer
                     System.Drawing.GraphicsUnit.Pixel);
 
                 grPhoto = System.Drawing.Graphics.FromImage(tile_LOD);
-                grPhoto.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                grPhoto.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
 
                 grPhoto.DrawImage(tile,
                     new System.Drawing.Rectangle(0, 0, tile_LOD.Width, tile_LOD.Height),
